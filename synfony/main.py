50fa21e5f612c07bcb15afb9db63b39ee51436e1
William McInroy <mcinroy@college.harvard.edu>
2023-05-01 11:10:43 -0400

    ok, most of the handshake minus client state + sync

--- synfony/main.py
+++ synfony/main.py
@@ -12,10 +12,10 @@ from synfony.models import MachineAddress
 from synfony.models import IdentityRequest, IdentityResponse
 from synfony.models import HeartbeatRequest, HeartbeatResponse
 from synfony.ui import initUI
-from threading import Thread
 from typing import List, Tuple
 
 
+import threading
 import time
 
 
@@ -116,6 +116,9 @@ def metronome(other_sockets: List[socket],
         are timing out in `Config.HANDSHAKE_TIMEOUT`, whcih are much faster
         than `Config.HEARTBEAT_TIMEOUT`.
     """
+    start_t = time.time()
+    start_t_ms = int(1000 * start_t)
+
     # TODO: locking
     machine_addresses=[machine
                        for i, machine in
@@ -139,8 +142,6 @@ def metronome(other_sockets: List[socket],
         realtime=0
     )
 
-    start_t = time.time()
-    start_t_ms = int(1000 * start_t)
     request = HeartbeatRequest(
         channel_events_states=[event],
         machine_addresses=machine_addresses,
@@ -162,11 +163,46 @@ def metronome(other_sockets: List[socket],
         other_machine_addresses[i].set_status(status)
     # TODO: start threads
 
-    # TODO: 2 - pull off queue, wait until acceptable
-    # TODO: failures for next state
-    # TODO: 3 - consensus + `ui_manager.streamer.sync(...)`
-    # TODO: 4 - schedule next
-    pass
+    # 2 - pull off queue, wait until acceptable
+    def message_queues_empty():
+        return False
+
+    while (time.time() - start_t < Config.HEARTBEAT_TIMEOUT and
+           message_queues_empty()):
+        time.sleep(Config.HANDSHAKE_TIMEOUT)
+
+    # TODO: join threads from before
+
+    votes: List[HeartbeatRequest] = []
+    for i, queue in enumerate(machine_message_queues):
+        other_machine_addresses[i].set_status(len(queue) != 0 and
+                                              other_machine_addresses[i]
+                                              .get_status())
+        if len(queue) != 0:
+            votes.append(queue[-1])
+
+        machine_message_queues[i].clear()
+
+    # 3 - consensus + `ui_manager.streamer.sync(...)`
+    all_channel_idxes = {event.get_channel_state().get_idx()
+                         for vote in votes
+                         for event in vote.get_channel_events_states()}
+
+    # TODO: update channel states in `ui_manager`
+    for channel_idx in all_channel_idxes:
+        new_channel_state = consensus([event
+                                       for vote in votes
+                                       for event in
+                                       vote.get_channel_events_states()
+                                       if (event.get_channe_state()
+                                           .get_idx() == channel_idx)])
+    # TODO: call `ui.manager.streamer.sync(...)`
+
+    # 4 - schedule next
+    # TODO: correct arguments...
+    timer = threading.Timer(interval=(Config.HANDSHAKE_INTERVAL - start_t),
+                            function=handshake)
+    timer.start()
 
 
 def handler(e, s: socket):
@@ -194,8 +230,8 @@ def main(idx: int, machines: List[str]):
         s.settimeout(None)
         time.sleep(Config.TIMEOUT)
         # this should be `machine_addresses: List[MachineAddress]`
-        Thread(target=accept_clients,
-               args=(other_machine_addresses, s)).start()
+        threading.Thread(target=accept_clients,
+                         args=(other_machine_addresses, s)).start()
         # TODO: start timer for handshake
         time.sleep(Config.TIMEOUT)
         other_sockets = []
@@ -203,7 +239,10 @@ def main(idx: int, machines: List[str]):
             other_socket = socket(AF_INET, SOCK_STREAM)
             other_socket.settimeout(None)  # TODO handshake timeout post start
             other_socket.connect(other_machine_address)
-            # other_socket.sendall(IdentityRequest(idx=idx).serialize())
+            # other_socket.sendall(IdentityRequest(
+            #     idx=idx,
+            #     machine_address=machine_address).serialize()
+            # )
             other_sockets.append(other_socket)
         initUI()
     except Exception as e:
