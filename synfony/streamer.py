61a5e365a98b3d6369ea42a96dde7c4543ec0890
Max Snyder <maxsnyder2000@gmail.com>
2023-05-01 10:55:06 -0400

    seek

--- synfony/streamer.py
+++ synfony/streamer.py
@@ -2,9 +2,9 @@
 # in synfony
 
 from abc import ABC, abstractmethod
-from datetime import datetime
 from synfony.config import Config
 from synfony.models import ChannelState
+from time import time
 
 import pygame
 
@@ -20,6 +20,10 @@ class Streamer(ABC):
 
     @abstractmethod
     def get_current_time(self, channel_id):
+        pass
+
+    @abstractmethod
+    def get_num_channels(self):
         pass
 
     @abstractmethod
@@ -39,7 +43,7 @@ class Streamer(ABC):
         pass
 
     @abstractmethod
-    def sync(self, state):
+    def sync(self, states):
         pass
 
     @abstractmethod
@@ -62,7 +66,7 @@ class LocalMusicStreamer(Streamer):
             channel.set_endevent(pygame.USEREVENT + channel_id)
             channel.play(sound)
             self.current_chunk_index.append(1)
-            self.current_chunk_realtime.append(datetime.now())
+            self.current_chunk_realtime.append(time())
             self.current_chunk_timestamp.append(0.0)
             self.playing.append(True)
             self.volume.append(50)
@@ -75,20 +79,24 @@ class LocalMusicStreamer(Streamer):
         chunk = self.current_chunk_index[channel_id] + 1
         if chunk > Config.CHANNELS[channel_id][1]:
             chunk = 1
-        self.current_chunk_index[channel_id] = chunk
-        chunk = str(chunk) if chunk > 9 else "0" + str(chunk)
-        sound = pygame.mixer.Sound(file + "-" + chunk + ".mp3")
+        chunk_str = str(chunk) if chunk > 9 else "0" + str(chunk)
+        sound = pygame.mixer.Sound(file + "-" + chunk_str + ".mp3")
         channel = pygame.mixer.Channel(channel_id)
         channel.queue(sound)
-        self.current_chunk_realtime[channel_id] = datetime.now()
+        self.current_chunk_index[channel_id] = chunk
+        self.current_chunk_realtime[channel_id] = time()
         self.current_chunk_timestamp[channel_id] = 0.0
-        self.playing.append(True)
 
     def get_current_time(self, channel_id):
         inter_chunk_offset = (self.current_chunk_index[channel_id] - 1) * Config.CHANNELS[channel_id][2]
         intra_chunk_offset = self.current_chunk_timestamp[channel_id]
-        realtime_offset = datetime.now() - self.current_chunk_realtime[channel_id]
-        return inter_chunk_offset + intra_chunk_offset + realtime_offset.total_seconds()
+        realtime_offset = time() - self.current_chunk_realtime[channel_id]
+        if not self.playing[channel_id]:
+            realtime_offset = 0.0
+        return inter_chunk_offset + intra_chunk_offset + realtime_offset
+
+    def get_num_channels(self):
+        return len(Config.CHANNELS)
 
     def get_title(self, channel_id):
         return Config.CHANNELS[channel_id][0]
@@ -102,16 +110,37 @@ class LocalMusicStreamer(Streamer):
     def is_playing(self, channel_id):
         return self.playing[channel_id]
 
-    def sync(self, state: list[ChannelState]):
-        for channel_state in state:
+    def sync(self, states: list[ChannelState]):
+        for channel_state in states:
             channel_id = channel_state.get_idx()
             play = channel_state.get_playing()
             timestamp = channel_state.get_timestamp()
+            chunk = 1
+            while timestamp > Config.CHANNELS[channel_id][2]:
+                timestamp -= Config.CHANNELS[channel_id][2]
+                chunk += 1
             channel = pygame.mixer.Channel(channel_id)
             if self.playing[channel_id] and not play:
+                self.current_chunk_index[channel_id] = chunk
+                self.current_chunk_timestamp[channel_id] = timestamp
                 channel.pause()
             elif not self.playing[channel_id] and play:
+                self.current_chunk_index[channel_id] = chunk
+                self.current_chunk_timestamp[channel_id] = timestamp
                 channel.unpause()
+                self.current_chunk_realtime[channel_id] = time()
+            elif abs(timestamp - self.get_current_time(channel_id)) > Config.TOLERABLE_DELAY:
+                self.current_chunk_index[channel_id] = chunk
+                self.current_chunk_timestamp[channel_id] = 0.0 # timestamp
+                file = Config.CHANNELS[channel_id][0]
+                chunk_str = str(chunk) if chunk > 9 else "0" + str(chunk)
+                sound = pygame.mixer.Sound(file + "-" + chunk_str + ".mp3")
+                channel = pygame.mixer.Channel(channel_id)
+                channel.play(sound)
+                if self.playing[channel_id]:
+                    self.current_chunk_realtime[channel_id] = time()
+                else:
+                    channel.pause()
             self.playing[channel_id] = play
 
     def shutdown(self):
